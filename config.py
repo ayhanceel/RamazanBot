8e6560b7b5535a2231877bc19771fbd832bb28be
ayhanceel <126969278+ayhanceel@users.noreply.github.com>
2023-04-03 16:58:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-API_HASH = os.environ.get("API_HASH", None)
-API_ID = os.environ.get("API_ID", None)
-OWNER_ID = os.environ.get("OWNER_ID", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6060382452:AAGZep2wL0Ss81UUnw-N5sJkuiTvxVYXd-8")
+API_HASH = os.environ.get("API_HASH", "40ee7035ccf23619001b9d933bb6b6e1")
+API_ID = os.environ.get("API_ID", "28842938")
+OWNER_ID = os.environ.get("OWNER_ID", "cavres34")
